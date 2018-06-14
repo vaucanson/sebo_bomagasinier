fa4634173588d16c346df84bc0b5bbd8e2621917
dav <moi@mondomaine.tld>
2018-06-14 09:35:52 +0200

    les dto utiles pour le produit

--- src/java/tools/JsonTools.java
+++ src/java/tools/JsonTools.java
@@ -6,8 +6,10 @@
 package tools;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import dto.ClientOrder;
+import dto.Product;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -22,21 +24,51 @@ import jdk.nashorn.api.scripting.URLReader;
  */
 public class JsonTools {
     
-    public static List<ClientOrder> getClientOrdersFromJson(URL url)
-    {
-         Gson gson = new Gson();
-         List<ClientOrder> list = new ArrayList<>();
-
+//    public static List<ClientOrder> getClientOrdersFromJson(URL url)
+//    {
+//         Gson gson = new Gson();
+//         List<ClientOrder> list = new ArrayList<>();
+//
+//        try (Reader reader = new URLReader(url)) {
+//
+//            // Convert JSON to Java Object
+//            JsonArray ja = gson.fromJson(reader, JsonArray.class);
+//            
+//            for (JsonElement je : ja)
+//            {
+//                ClientOrder co = gson.fromJson(reader, ClientOrder.class);
+//                list.add(co);
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        
+//        return list;
+//    }
+    
+    
+    
+    
+    public static List<Product> getProductsFromJson(URL url) {
+        Gson gson = new Gson();
+        List<Product> myList = new ArrayList<Product>();
+        
         try (Reader reader = new URLReader(url)) {
 
             // Convert JSON to Java Object
-            list = gson.fromJson(reader, List.class);
+            JsonArray ja = gson.fromJson(reader, JsonArray.class);
+            for (JsonElement je : ja)
+            {
+                Product prod = gson.fromJson(reader, Product.class);
+                myList.add(prod);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
         
-        return list;
+        return myList;
     }
         
     
